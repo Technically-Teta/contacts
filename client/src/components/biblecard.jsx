16c61d54b31586d3e0c24e70acf61ffa1077b2b2
tushort <samquioh@gmail.com>
2023-10-03 13:44:09 -0700

    removed proptypes from eslint in biblecard

--- client/src/components/biblecard.jsx
+++ client/src/components/biblecard.jsx
@@ -1,21 +1,19 @@
-import Card from 'react-bootstrap/Card';
+/* eslint-disable react/prop-types */
+
 
     const BibleCard = (props) => {
     
         return (
           <div className={"bible-section"}>
-            <Card>     
+               
                                                             
-            <Card.Body><div className='book-text'>{props.verses.book_name}</div></Card.Body>
-            <Card.Body><div className='answer-section'>{props.verses.chapter}</div></Card.Body>
+             <div className='book-text'>{props.verses.book_name}</div>
+             <div className='answer-section'>{props.verses.chapter}</div>
           
             
-            </Card>
+           
           </div>
         );
       };
     
     export default BibleCard;
-
-
-
